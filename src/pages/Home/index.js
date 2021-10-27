a0efd422090f09dcb0534b1d037191628542416c
whitegod <kmbagwu12@gmail.com>
2021-10-27 21:57:28 +0100

    added react-rellax 3d effects

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+import Parallax from "react-rellax";
 import arrowSm from "../../assets/arrow-sm-left.svg";
 
 const Home = () => {
@@ -7,25 +8,29 @@ const Home = () => {
     <div className="app__home">
       <section className="section-intro">
         <div className="intro__content-box">
-          <span className="heading-caption">Hi there👋</span>
-          <h1 className="heading-main">
-            <span>I am</span>
-            <span>Whitegod</span>
-          </h1>
+          <Parallax speed={-1}>
+            <span className="heading-caption">Hi there👋</span>
+            <h1 className="heading-main">
+              <span>I am</span>
+              <span>Whitegod</span>
+            </h1>
+          </Parallax>
         </div>
       </section>
 
       <section className="section-about">
         <div className="about__content-box">
-          <span className="heading-caption">Developer👨‍💻</span>
-          <h2 className="heading-primary uti-margin-b">
-            About Me
-            <img src={arrowSm} alt="arrow-icon" className="about__icon" />
-          </h2>
-          <p className="description-primary">
-            I believe in diverse skill sets and people as they make products and
-            communities unique.
-          </p>
+          <Parallax speed={2}>
+            <span className="heading-caption">Developer👨‍💻</span>
+            <h2 className="heading-primary uti-margin-b">
+              About Me
+              <img src={arrowSm} alt="arrow-icon" className="about__icon" />
+            </h2>
+            <p className="description-primary">
+              I believe in diverse skill sets and people as they make products
+              and communities unique.
+            </p>
+          </Parallax>
         </div>
       </section>
     </div>
