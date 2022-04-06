4944a0e94c8247be86b1012ca2d54270a3628101
whitegod <kmbagwu12@gmail.com>
2022-04-06 13:58:01 +0530

    built the blog post secttion and started working on the newsletter form

--- src/pages/Blog/index.js
+++ src/pages/Blog/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 
 import avatar from "../../assets/whitegod_avatar.png";
+import blog_thumbnail from "../../assets/blog_img.png";
 
 const Blog = () => {
   return (
     <div className="app__blog">
-      <div className="blog__header">
+      <header className="blog__header">
         <div className="blog__header-text">
-          <h3>Hello, welcome to my blog</h3>
+          <h2>Hello, welcome to my blog</h2>
           <p>
             I’m Whitegod Kingsley, a frontend engineer. A clean code advocate,
             builds scalable and maintainable codebase, team player with an
@@ -21,7 +22,95 @@ const Blog = () => {
         <div className="blog__header-avatar">
           <img src={avatar} alt="Whitegod's avatar" />
         </div>
-      </div>
+      </header>
+
+      <section className="blog__posts">
+        <div className="blog__post-grid">
+          <div className="blog__post-card">
+            <img src={blog_thumbnail} alt="post thumbnail" />
+            <h2>How To Fund open source for FREE</h2>
+            <div className="blog__datetime">
+              <span>Dec 6, 2020</span>
+              <span>5 mins Read</span>
+            </div>
+            <p>
+              This article aims to give everyone more insight into why open
+              source is not Free. Depending on which track got you into open
+              source, we all know one thing in common “collaboration.” It is
+              important because this makes the open source model great. So...
+            </p>
+          </div>
+          <div className="blog__post-card">
+            <img src={blog_thumbnail} alt="post thumbnail" />
+            <h2>How To Fund open source for FREE</h2>
+            <div className="blog__datetime">
+              <span>Dec 6, 2020</span>
+              <span>5 mins Read</span>
+            </div>
+            <p>
+              This article aims to give everyone more insight into why open
+              source is not Free. Depending on which track got you into open
+              source, we all know one thing in common “collaboration.” It is
+              important because this makes the open source model great. So...
+            </p>
+          </div>
+          <div className="blog__post-card">
+            <img src={blog_thumbnail} alt="post thumbnail" />
+            <h2>How To Fund open source for FREE</h2>
+            <div className="blog__datetime">
+              <span>Dec 6, 2020</span>
+              <span>5 mins Read</span>
+            </div>
+            <p>
+              This article aims to give everyone more insight into why open
+              source is not Free. Depending on which track got you into open
+              source, we all know one thing in common “collaboration.” It is
+              important because this makes the open source model great. So...
+            </p>
+          </div>
+          <div className="blog__post-card">
+            <img src={blog_thumbnail} alt="post thumbnail" />
+            <h2>How To Fund open source for FREE</h2>
+            <div className="blog__datetime">
+              <span>Dec 6, 2020</span>
+              <span>5 mins Read</span>
+            </div>
+            <p>
+              This article aims to give everyone more insight into why open
+              source is not Free. Depending on which track got you into open
+              source, we all know one thing in common “collaboration.” It is
+              important because this makes the open source model great. So...
+            </p>
+          </div>
+          <div className="blog__post-card">
+            <img src={blog_thumbnail} alt="post thumbnail" />
+            <h2>How To Fund open source for FREE</h2>
+            <div className="blog__datetime">
+              <span>Dec 6, 2020</span>
+              <span>5 mins Read</span>
+            </div>
+            <p>
+              This article aims to give everyone more insight into why open
+              source is not Free. Depending on which track got you into open
+              source, we all know one thing in common “collaboration.” It is
+              important because this makes the open source model great. So...
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <section className="newsletter__section">
+        <form action="">
+          <p>Never miss upcoming my articles on Frontend & Tech in general</p>
+          <input
+            type="email"
+            placeholder="Email address"
+            name="email"
+            id="email"
+          />
+          <button type="submit">Subscribe</button>
+        </form>
+      </section>
     </div>
   );
 };
